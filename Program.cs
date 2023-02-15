a3290ef028a297522f6630b3b3bf40b58fe1796b
hialmarwarren <123904804+hialmarwarren@users.noreply.github.com>
2023-02-15 23:45:22 +0100

    Excercise1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Wprowadź z klawiatury dowolną liczbę rzeczywistą:   ");
+string number = Console.ReadLine();
+float numberToFloat = float.Parse(number);
+if (numberToFloat == 0)
+{
+    Console.WriteLine("Wprowadzona liczba jest równa 0.");
+}
+else if (numberToFloat < 0)
+{
+    Console.WriteLine("Wprowadzona liczba jest ujemna.");
+}
+else if (numberToFloat > 0)
+{
+    Console.WriteLine("Wprowadzona liczba jest dodatnia.");
+}
