43ed57c0be81be4b5a3dc9038f5da59085891e8c
hialmarwarren <123904804+hialmarwarren@users.noreply.github.com>
2023-02-16 22:16:42 +0100

    Ćwiczenie dni na lata, tygodnie, dni

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
-﻿Console.WriteLine("Wprowadź z klawiatury dowolną liczbę rzeczywistą:   ");
+﻿Console.WriteLine("Wprowadź z klawiatury dowolną liczbę dni:   ");
 string number = Console.ReadLine();
-float numberToFloat = float.Parse(number);
-if (numberToFloat == 0)
-{
-    Console.WriteLine("Wprowadzona liczba jest równa 0.");
-}
-else if (numberToFloat < 0)
-{
-    Console.WriteLine("Wprowadzona liczba jest ujemna.");
-}
-else if (numberToFloat > 0)
-{
-    Console.WriteLine("Wprowadzona liczba jest dodatnia.");
-}
+int numberOfDays = int.Parse(number);
+int y, t, d;
+y = numberOfDays / 365;
+t = (numberOfDays-y*365)  / 7;
+d = numberOfDays - y*365 - t*7;
+//lata
+Console.WriteLine("To " + numberOfDays / 365+" pełnych l");
+//tygodnie
+Console.WriteLine("lub " + numberOfDays / 7 + " pełnych t");
+//dni
+Console.WriteLine("lub "+numberOfDays+ " pełnych d");
+Console.WriteLine();
+Console.WriteLine("Co daje sumarycznie: "+y+" l, "+t+" t i "+ d+" d");
+
